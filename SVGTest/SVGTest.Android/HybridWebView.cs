0a4bec526cfa6fef43ba08c377834b37321213a1
ElennMayAdeliine <33444865+ElennMayAdeliine@users.noreply.github.com>
2019-03-25 19:54:18 +0200

    ules

---
+++ SVGTest/SVGTest.Android/HybridWebView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace SVGTest.Droid
+{
+    public class HybridWebView : View
+    {
+        Action<string> action;
+        public static readonly BindableProperty UriProperty = BindableProperty.Create(
+          propertyName: "Uri",
+          returnType: typeof(string),
+          declaringType: typeof(HybridWebView),
+          defaultValue: default(string));
+
+        public string Uri
+        {
+            get { return (string)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
+        }
+
+        public void RegisterAction(Action<string> callback)
+        {
+            action = callback;
+        }
+
+        public void Cleanup()
+        {
+            action = null;
+        }
+
+        public void InvokeAction(string data)
+        {
+            if (action == null || data == null)
+            {
+                return;
+            }
+            action.Invoke(data);
+        }
+    }
+}
---
+++ SVGTest/SVGTest/ItemsId.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SVGTest
+{
+    class ItemsId
+    {
+        public void GetElements()
+        {
+
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load(@"TestGraafika2.svg");
+            //XmlNodeList elementList = doc.GetElementsByTagName("rect");
+            //string[] labels = new string[elementList.Count];
+            //for (int i = 0; i < elementList.Count; i++)
+            //{
+            //    int id = int.Parse(elementList[i].Attributes["id"].Value);
+            //    labels[id] = elementList[i].Attributes["inkspace:label"].Value;
+            //}
+
+            XDocument xdoc = XDocument.Load(@"TestGraafika2.svg");
+            var elements = xdoc.Root.Elements().Select(e => new
+            {
+                id = e.Attributes("id").FirstOrDefault()?.Value ?? "",
+                type = e.Name,
+                x = e.Attributes("x").FirstOrDefault()?.Value ?? "",
+                y = e.Attributes("y").FirstOrDefault()?.Value ?? "",
+                w = e.Attributes("width").FirstOrDefault()?.Value ?? "",
+                h = e.Attributes("height").FirstOrDefault()?.Value ?? "",
+                element = e
+            });
+
+            if (elements.Count() > 0)
+            {
+
+            }
+        }
+    }
+            
+            
+                
+}
+    
+
+
